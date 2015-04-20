bcd9b8b08ccc78889ab7994cb55addc14651c1b3
ijd2104 <ijd2104@columbia.edu>
2015-04-20 19:55:07 -0400

    modified image code to get full res images

--- googleimage.m
+++ googleimage.m
@@ -4,15 +4,20 @@ function googleimage(name)
     hexcode=dec2hex(double(name));
     percents=repmat('%',size(hexcode,1),1);
     urlstr=sprintf('%s',strcat(percents,hexcode)');
-html = urlread(['http://images.google.com/images?q=' urlstr]);
+    urlstr = strcat(urlstr,'&imgsz=large');
+%html = urlread(['http://images.google.com/images?q=' urlstr]);
+html = urlread(['http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' urlstr]);
 %display(html);
 %html = urlread(['http://www.bing.com/images/search?q=' urlstr]);
 %moon = findstr('',html);
 %display(moon);
-beg = findstr('src="http:',html);
-begend = html(beg:beg+200);
+%beg = findstr('src="http:',html);
+beg = findstr('"url":"',html);
+%begend = html(beg:beg+200);
+begend = html(beg+7:end);
 %display(begend);
-fin = findstr('" width="',begend);
+%fin = findstr('" width="',begend);
+fin = findstr('","',begend);
 %disp(beg);
 %disp(fin);
 %beg1 = beg(1);
@@ -21,6 +26,23 @@ fin = findstr('" width="',begend);
 %disp(html(beg:bege));
 %disp(html(beg+5:fin-1));
 %filename= websave('urlimage', ['http://images.google.com/images?q=' argin]);
-A = imread(begend(6:fin-1));
-figure, imshow(A,'Border','tight');
+try
+    A = imread(begend(1:fin-1));
+catch
+    try
+        beg = findstr('"url":"',begend);
+        begend = begend(beg+7:end);
+        fin = findstr('","',begend);
+        A = imread(begend(1:fin-1));
+    catch
+        beg = findstr('"url":"',begend);
+        begend = begend(beg+7:end);
+        fin = findstr('","',begend);
+        A = imread(begend(1:fin-1));
+    end
+end
+    
+figure('MenuBar','none','NumberTitle','off','Name',name)
+%set(0,'DefaultFigureMenu','none');
+imshow(A,'Border','tight');
 end
